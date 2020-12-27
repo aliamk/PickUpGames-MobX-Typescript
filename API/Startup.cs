190866c6f9c77fce240e7d1eb4057bef4d521068
unknown <alia.azarian@gmail.com>
2020-12-27 01:12:50 +0000

    Initial migration created

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;                                     // DbContext
 
 namespace API
 {
@@ -25,7 +26,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DbContext>(opt =>
+            services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
