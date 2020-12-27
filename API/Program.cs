34424a169125b0c0219a52693deb629f666e56fe
unknown <alia.azarian@gmail.com>
2020-12-27 01:21:28 +0000

    Updated Program.cs so now it checks for a database and creates one if there isn't one already... it's created a pinga.db file in API/

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;                // Migrate
+using Microsoft.Extensions.DependencyInjection;     // CreateScope
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;                                  // DataContext
+using Persistence.Migrations;
 
 namespace API
 {
@@ -13,7 +13,23 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())     // access datacontext
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    // Seed.SeedData(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred during migration");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
