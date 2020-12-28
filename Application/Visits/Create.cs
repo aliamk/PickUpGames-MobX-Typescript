62fff2cdd7c2f26e73836955362543f14fe7bee8
unknown <alia.azarian@gmail.com>
2020-12-28 17:59:42 +0000

    Added Create.cs handler to Application > Visits which allows the creation of new Visits items by the user

---
+++ Application/Visits/Create.cs
@@ -0,0 +1,51 @@
+using System;                   // Guid + DateTime
+using System.Threading;         // IRequestHandler
+using System.Threading.Tasks;   // IRequestHandler
+using Domain;                   // Visit
+using MediatR;                  // IRequest
+using Persistence;              // DataContext
+
+// FOR USERS TO ADD A VISIT ITEM TO THE DATABASE
+
+namespace Application.Visits
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public String Title { get; set; }
+            public String Description { get; set; }
+            public DateTime Date { get; set; }
+            public String Location { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var visit = new Visit
+                {
+                    Id = request.Id,
+                    Title = request.Title,
+                    Description = request.Description,
+                    Date = request.Date,
+                    Location = request.Location
+                };
+                _context.Visits.Add(visit);
+
+                // If SaveChangesAsync returns a value more than 0, this means user has 
+                // successfully added an item to the database and we just want to return that value
+                var success = await _context.SaveChangesAsync() > 0;
+                if (success) return Unit.Value;
+                //  Else, return this error message
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
