b0f954cad6e52f31e07af4bb67a6aacf709967ec
unknown <alia.azarian@gmail.com>
2020-12-28 17:32:57 +0000

    Created first handler in Application > Visits > List in order to access via http request the list of visits

---
+++ Application/Visits/List.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;       // List
+using System.Threading;                 // IRequestHandler (Implement Interface)
+using System.Threading.Tasks;           // IRequestHandler (Implement Interface)
+using Domain;                           // Visit
+using MediatR;                          // IRequest
+using Microsoft.EntityFrameworkCore;    // ToListAsync
+using Persistence;                      // DataContext
+
+namespace Application.Visits
+{
+    public class List
+    {
+        public class Query : IRequest<List<Visit>> { }
+        public class Handler : IRequestHandler<Query, List<Visit>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Visit>> Handle(Query request,
+                CancellationToken cancellationToken)
+            {
+                var visits = await _context.Visits.ToListAsync();
+                return visits;
+            }
+        }
+    }
+}
