d6a736af7436f1eeed488102d50396878ae85885
unknown <alia.azarian@gmail.com>
2020-12-28 16:18:24 +0000

    Updated Startup.cs with Cors policy as a service and middleware

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,15 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -46,6 +55,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import "./App.css"
+import 'semantic-ui-css/semantic.min.css'
+import axios from 'axios'
+import { Header, Icon, List } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// interface IState {
+//   values: []
+// }
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+      // console.log(response)
+      this.setState({
+        values: response.data
+      })
+    })    
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Pinga</Header.Content>
+        </Header>
+
+        <List>
+            {this.state.values.map((value) => (
+              <List.Item key={ value.Id }>{ value.name }</List.Item>
+            ))}
+        </List>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App 
