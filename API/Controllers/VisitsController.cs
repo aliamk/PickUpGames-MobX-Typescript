849ce7d937dcf7f655fe7afb7ca7a33b5d95f200
unknown <alia.azarian@gmail.com>
2020-12-28 18:01:23 +0000

    Updated ValuesController.cs with a Post command for users to post new Visit items to the database

--- API/Controllers/VisitsController.cs
+++ API/Controllers/VisitsController.cs
@@ -33,27 +33,27 @@ namespace API.Controllers
         {
             return await _mediator.Send(new Details.Query { Id = id }); // initialise Id 
         }
-        /*
+
         // For users to add their own Visit item to the database - see the Create Handler in Create.cs
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await _mediator.Send(command);
         }
+        /*
+                //  For users to edit a database item
+                [HttpPut("{id}")]
+                public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+                {
+                    command.Id = id;
+                    return await _mediator.Send(command);
+                }
 
-        //  For users to edit a database item
-        [HttpPut("{id}")]
-        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
-        {
-            command.Id = id;
-            return await _mediator.Send(command);
-        }
-
-        // For users to delete database items 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Unit>> Delete(Guid id)
-        {
-            return await _mediator.Send(new Delete.Command { Id = id });
-        } */
+                // For users to delete database items 
+                [HttpDelete("{id}")]
+                public async Task<ActionResult<Unit>> Delete(Guid id)
+                {
+                    return await _mediator.Send(new Delete.Command { Id = id });
+                } */
     }
 }
