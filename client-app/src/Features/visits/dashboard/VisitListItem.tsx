a66b91398bb78782116e169ad7181be28c57e870
unknown <alia.azarian@gmail.com>
2020-12-29 23:35:01 +0000

    Added styling from Semanti-UI-React to the VisitListItem component

--- client-app/src/Features/visits/dashboard/VisitListItem.tsx
+++ client-app/src/Features/visits/dashboard/VisitListItem.tsx
@@ -1,39 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Item, Label, Button } from 'semantic-ui-react'
+import { Item, Label, Button, Segment, Icon } from 'semantic-ui-react'
 import { IVisit } from '../../../App/models/visit_interface'
 
 
 const VisitListItem: React.FC<{ visit: IVisit }> = ({ visit }) => {
 
     return (
-        <Item key={visit.id}>        
-        <Item.Content>
-            <Item.Header as='a'>{visit.title}</Item.Header>
-            <Item.Meta>{visit.date}</Item.Meta>
-            <Item.Description>
-                <div>{visit.description}</div>
-                <div>{visit.location}</div>
-            </Item.Description>
-            <Item.Extra>
+        <Segment.Group>
+            
+            <Segment>
+                <Item>
+                    <Item.Image size='tiny' circular src='assets/user.png' />        
+                <Item.Content>
+                    <Item.Header as='a'>{visit.title}</Item.Header>
+                    <Item.Description>Hosted by Bob</Item.Description>
+                </Item.Content>
+                </Item>
+            </Segment>
+
+            <Segment>
+                <Icon name='clock' />{visit.date}
+                <Icon name='marker' />{visit.location}
+            </Segment>
+
+            <Segment secondary>
+                Attendees will go here
+            </Segment>
+
+            <Segment clearing>
+                <span>{visit.description}</span>
                 <Button 
                     as={Link} to={`/visits/${visit.id}`}
                     floated='right' 
                     content='View' 
                     color='blue' 
-                />
-                {/*<Button 
-                    name={visit.id}
-                    loading={target === visit.id && submitting} 
-                    onClick={(e) => deleteVisit(e, visit.id)} 
-                    floated='right' 
-                    content='Delete' 
-                    color='red' 
-                />*/}
-                <Label basic content='category' />
-            </Item.Extra>
-        </Item.Content>
-    </Item>
+                />   
+            </Segment>
+        </Segment.Group>
     )
 }
 
