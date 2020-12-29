c3ea675a028e81c8a46611042d522b163d9b419b
unknown <alia.azarian@gmail.com>
2020-12-29 13:33:47 +0000

    Converted App.tsx into an observer of the activityStore

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react'
+import React, { useState, useEffect, Fragment, SyntheticEvent, useContext } from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react'
 import { IVisit } from '../models/visit_interface'
@@ -6,9 +6,14 @@ import NavBar from '../../Features/nav/NavBar'
 import VisitDashboard from '../../Features/visits/dashboard/VisitDashboard'
 import agent from '../api/agent'
 import LoadingComponent from '../layout/LoadingComponent'
+import VisitStore from '../stores/visitStore'
+import {observer} from 'mobx-react-lite'
 
 
 const App = () => {
+
+  const visitStore = useContext(VisitStore)
+
   const [ visits, setVisits ] = useState<IVisit[]>([])
   const [ selectedVisit, setSelectedVisit ] = useState<IVisit | null>(null)
   const [ editMode, setEditMode ] = useState(false)
@@ -56,26 +61,17 @@ const App = () => {
   
   // ========  API CALLS ======== //
   useEffect(() => {
-    agent.Visits.list()    
-    .then((response) => {
-      // console.log(response)
-      let visits: IVisit[] = []
-      response.forEach(visit => {
-        visit.date = visit.date.split('.')[0]
-        visits.push(visit)
-      })
-      setVisits(visits)
-    }).then(() => setLoading(false))
-  }, [])
+    visitStore.loadVisits()
+  }, [visitStore])
 
-  if (loading) return <LoadingComponent content='Loading Visits...' />
+  if (visitStore.loadingInitial) return <LoadingComponent content='Loading Visits...' />
   
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{marginTop: '7em'}}>
         <VisitDashboard 
-          visits={visits} 
+          visits={visitStore.visits} 
           selectVisit={handleSelectVisit} 
           selectedVisit={selectedVisit}
           editMode={editMode}
@@ -92,4 +88,4 @@ const App = () => {
   )
 }
 
-export default App 
+export default observer(App) 
--- client-app/src/App/stores/visitStore.ts
+++ client-app/src/App/stores/visitStore.ts
@@ -1,9 +1,24 @@
-import { observable } from 'mobx'
+import { observable, action } from 'mobx'
 import { createContext } from 'react'
+import agent from '../api/agent'
+import { IVisit } from '../models/visit_interface'
 
 
 class VisitStore {
-    @observable title = 'Helloooooo'
+    @observable visits:IVisit[] = []
+    @observable loadingInitial = false 
+
+    @action loadVisits = () => {
+        this.loadingInitial = true                  // mutating state with MobX
+        agent.Visits.list()    
+        .then(visits => {
+            // console.log(response)
+            visits.forEach(visit => {
+                visit.date = visit.date.split('.')[0]
+                this.visits.push(visit)
+            })
+        }).finally(() => this.loadingInitial = false)
+    }
 }
 
 export default createContext(new VisitStore())
