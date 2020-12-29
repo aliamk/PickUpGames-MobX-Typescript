15e9d7d578568ed0d686a84cd995929f3dd36f9b
unknown <alia.azarian@gmail.com>
2020-12-29 02:32:42 +0000

    Added functionality for the Add a Visit form to be writable

--- client-app/src/Features/visits/form/VisitForm.tsx
+++ client-app/src/Features/visits/form/VisitForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
 import { Form, Segment, Button } from 'semantic-ui-react'
 import { IVisit } from '../../../App/models/visit_interface'
 
@@ -24,13 +24,22 @@ const VisitForm:React.FC<IProps> = ({ setEditMode, visit: initialFormState }) =>
 
     const [ visit, setVisit ] = useState<IVisit>(initialiseForm)
 
+    const handleSubmit = () => {
+        console.log(visit)
+    }
+
+    const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = event.currentTarget
+        setVisit({ ...visit, [name]: value})
+    }
+
     return (
         <Segment clearing>
-            <Form>
-                <Form.Input placeholder='Title' value={visit.title} />
-                <Form.TextArea rows={2} placeholder='Description'  value={visit.description} />
-                <Form.Input type='date' placeholder='Date'  value={visit.date}/>
-                <Form.Input placeholder='Location' value={visit.location} />
+            <Form onSubmit={handleSubmit}>
+                <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={visit.title} />
+                <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description'  value={visit.description} />
+                <Form.Input onChange={handleInputChange} name='date' type='date' placeholder='Date'  value={visit.date}/>
+                <Form.Input onChange={handleInputChange} name='location' placeholder='Location' value={visit.location} />
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel' />
             </Form>            
