e7052ac16b18e77355b50bbc792a5591b047c04c
unknown <alia.azarian@gmail.com>
2020-12-30 14:50:49 +0000

    Commented out default developer exception page in Startup.cs and replaced it with the newly created custom middleware in the API folder: app.UseMiddleware<ErrorHandlingMiddleware>()

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using API.Middleware;                               // ErrorHandlingMiddleware
 using Application.Visits;                           // (typeof(List))
 using FluentValidation.AspNetCore;                  // AddFluentValidation
 using MediatR;                                      // AddMediatR
@@ -47,9 +48,10 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
             }
 
             // app.UseHttpsRedirection();
