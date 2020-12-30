bc6b1975e228445d14a80c0a604beeed18185a35
unknown <alia.azarian@gmail.com>
2020-12-30 01:17:38 +0000

    Added styling to the HomePage

--- client-app/src/Features/home/HomePage.tsx
+++ client-app/src/Features/home/HomePage.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, {Fragment } from 'react'
 import { Link } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import { Container, Segment, Header, Button, Image } from 'semantic-ui-react';
 
 const HomePage = () => {
     return (
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Go to <Link to='/visits'>Visits</Link></h3>
+        <Segment inverted textAlign='center' vertical className='masthead'>
+        <Container text>
+          <Header as='h1' inverted>
+            <Image
+              size='massive'
+              src='/assets/heart_logo.png'
+              alt='logo'              
+              style={{ marginBottom: 12 }}
+            />
+            Pinga
+          </Header>
+         
+            <Fragment>
+              {/* <Header as='h2' inverted content='Welcome back' /> */}
+              <Button as={Link} to='/visits' size='huge' inverted>
+                Catchup on Visits!
+              </Button>
+            </Fragment>
+
+            <Fragment>
+            <Header as='h2' inverted content={`Welcome to Pinga`} />
+            {/* <Button size='huge' inverted>
+              Login
+            </Button>
+            <Button size='huge' inverted>
+              Register
+            </Button> */}
+          </Fragment>
+
         </Container>
+      </Segment>
     )
 }
 
