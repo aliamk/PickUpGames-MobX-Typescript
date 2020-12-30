09d6a3d4db496027486bc3fe2ad247491dfdd562
unknown <alia.azarian@gmail.com>
2020-12-30 14:44:40 +0000

    Created custom erro middleware in API > Middleware > ErrorHandlingMiddlware.cs

---
+++ API/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,64 @@
+using System;                               // Exception
+using System.Net;                           // HttpStatusCode
+using System.Text.Json;                     // JsonSerializer
+using System.Threading.Tasks;               // Task
+using Application.Errors;                   // RestException
+using Microsoft.AspNetCore.Http;            // RequestDelegate
+using Microsoft.Extensions.Logging;         // ILogger
+
+// Custom Middleware
+namespace API.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex, _logger);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            object errors = null;
+
+            switch (ex)
+            {
+                case RestException re:
+                    logger.LogError(ex, "REST ERROR");
+                    errors = re.Errors;
+                    context.Response.StatusCode = (int)re.Code;
+                    break;
+                case Exception e:
+                    logger.LogError(ex, "SERVER ERROR");
+                    errors = string.IsNullOrWhiteSpace(e.Message) ? "Error" : e.Message;
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            if (errors != null)
+            {
+                var result = JsonSerializer.Serialize(new
+                {
+                    errors
+                });
+
+                await context.Response.WriteAsync(result);
+            }
+        }
+    }
+}
