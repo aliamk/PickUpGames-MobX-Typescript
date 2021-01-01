1af1be479db9225183c60e831439fb722918494a
unknown <alia.azarian@gmail.com>
2021-01-01 18:51:19 +0000

    Created a BaseController file in API > Controllers as a template because of the duplicate code shared by most controller I'll create (e.g., MediatR etc); then deleted the duplicate code from the VisitController and set it to read from BaseController

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -6,7 +6,6 @@ using Domain;                           // AppUser
 using FluentValidation;                 // AbstractValidator
 using MediatR;                          // IRequest
 using Microsoft.AspNetCore.Identity;    // UserManager
-using Persistence;                      // DataContext
 
 namespace Application.User
 {
@@ -16,7 +15,6 @@ namespace Application.User
         {
             public string Email { get; set; }
             public string Password { get; set; }
-
         }
 
         public class QueryValidator : AbstractValidator<Query>
@@ -30,27 +28,26 @@ namespace Application.User
 
         public class Handler : IRequestHandler<Query, AppUser>
         {
-            // private readonly DataContext _context;
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
             public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
             {
                 _signInManager = signInManager;
                 _userManager = userManager;
-                // _context = context;
             }
 
             public async Task<AppUser> Handle(Query request,
                 CancellationToken cancellationToken)
             {
-                // handler logic goes here
+                // Email handler logic
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
                 if (user == null)
-                    throw new RestException(HttpStatusCode.Unauthorized) // produces 401 error
+                    throw new RestException(HttpStatusCode.Unauthorized); // produces 401 error
 
+                // Password handler logic
+                var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
 
-                var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false)
                 if (result.Succeeded)
                 {
                     // TODO: generate token
