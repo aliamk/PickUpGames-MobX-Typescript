9fa72cd9255e58b59f59a3d79472dab029860716
unknown <alia.azarian@gmail.com>
2021-01-01 17:25:37 +0000

    Updated Startup.cs with Identity Builder service

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,9 +1,11 @@
 using API.Middleware;                               // ErrorHandlingMiddleware
 using Application.Visits;                           // (typeof(List))
+using Domain;                                       // AppUser
 using FluentValidation.AspNetCore;                  // AddFluentValidation
 using MediatR;                                      // AddMediatR
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;                // IdentityBuilder
 using Microsoft.EntityFrameworkCore;                // UseSqlite
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,11 +26,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Connect to the database
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-
+            // Solve cross-origin problems
             services.AddCors(opt =>
             {
                 opt.AddPolicy("CorsPolicy", policy =>
@@ -36,13 +39,19 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
-
+            // For decoupling asnyc handlers from requests
             services.AddMediatR(typeof(List.Handler).Assembly);
+            // Fluid Validation for checks before sending data to command handlers
             services.AddControllers()
                 .AddFluentValidation(cfg =>
             {
                 cfg.RegisterValidatorsFromAssemblyContaining<Create>();
             });
+            // Identity builder
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
