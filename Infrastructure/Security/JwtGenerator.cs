755b3d195842ff7ea03b0abaf57805a168fea911
unknown <alia.azarian@gmail.com>
2021-01-01 23:21:16 +0000

    Updated the JwtGenerator.cs file

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,6 +1,11 @@
-﻿using System;
-using Application.Interfaces;           // IJwtGenerator
-using Domain;                           // AppUser
+﻿using System;                               // DateTime
+using System.Collections.Generic;           // Claim
+using System.IdentityModel.Tokens.Jwt;      // JwtRegisteredClaimNames
+using System.Security.Claims;               // Claim
+using System.Text;
+using Application.Interfaces;
+using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
@@ -8,7 +13,27 @@ namespace Infrastructure.Security
     {
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            // Generate sign-in credentials - validate credentials w/o querying the database
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Super secret key"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+            // Generate and return the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
