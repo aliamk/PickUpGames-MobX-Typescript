04553b2557000903149304186e7891deaafdf7a1
unknown <alia.azarian@gmail.com>
2021-01-02 16:49:08 +0000

    Updated commonStore with MobX's 'reaction' which is triggered by the setToken action to check for a token and set it to localStorage (or discard token from localStorage if token is no longer being sent by action

--- client-app/src/App/stores/commonStore.ts
+++ client-app/src/App/stores/commonStore.ts
@@ -1,29 +1,29 @@
 import {RootStore} from './rootStore';
-import { observable, action } from 'mobx';
+import { observable, action, reaction } from 'mobx';
 
 export default class CommonStore {
     rootStore: RootStore;
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore;
 
-        // reaction(
-        //     () => this.token,
-        //     token => {
-        //         if (token) {
-        //             window.localStorage.setItem('jwt', token);
-        //         } else {
-        //             window.localStorage.removeItem('jwt')
-        //         }
-        //     }
-        // )
+        // reaction is triggered by setToken action; if token exists, set it to localStorage; else, remove it from lStorage
+        reaction(
+            () => this.token,
+            token => {
+                if (token) {
+                    window.localStorage.setItem('jwt', token);
+                } else {
+                    window.localStorage.removeItem('jwt')
+                }
+            }
+        )
     }
 
-    @observable token: string | null = null;
+    @observable token: string | null = window.localStorage.getItem('jwt');
     @observable appLoaded = false;
 
-    // Method to set the token
+    // Method to set the token - triggers reaction
     @action setToken = (token: string | null) => {
-        window.localStorage.setItem('jwt', token!)
         this.token = token;
     }
 
