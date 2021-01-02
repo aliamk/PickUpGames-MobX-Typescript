ad8b6fa80ebe5c7f4fec3868ce5555a94a268965
unknown <alia.azarian@gmail.com>
2021-01-02 00:54:49 +0000

    Added a temporary decorator to UserController in order to use the app without loggin in [AllowAnonymous]

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,12 +1,15 @@
-using Microsoft.AspNetCore.Mvc;         // HttpPost
-using System.Threading.Tasks;           // Task
-using Domain;                           // AppUser
-using Application.User;                 // Login
+using Microsoft.AspNetCore.Mvc;             // HttpPost
+using System.Threading.Tasks;               // Task
+using Domain;                               // AppUser
+using Application.User;                     // Login
+using Microsoft.AspNetCore.Authorization;   // AllowAnonymous
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     {
+
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
