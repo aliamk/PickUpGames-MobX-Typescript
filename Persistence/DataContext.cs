91f323c798b6c33bd0d8759d0497af2af7132c73
unknown <alia.azarian@gmail.com>
2021-01-03 00:27:28 +0000

    Updated DataContext.cs with access to the UserVisit database and two builder.entities added defining the Many to Many relationship between Users and Visits

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,7 @@ namespace Persistence
         }
         public DbSet<Value> Values { get; set; }
         public DbSet<Visit> Visits { get; set; }
+        public DbSet<UserVisit> UserVisits { get; set; }
 
         // Seed the database with initial values
         protected override void OnModelCreating(ModelBuilder builder)
@@ -23,6 +24,22 @@ namespace Persistence
                     new Value { Id = 2, Name = "Value 102" },
                     new Value { Id = 3, Name = "Value 103" }
                 );
+
+            // Define a primary key for UserVisits
+            builder.Entity<UserVisit>(x => x.HasKey(uv =>
+                new { uv.AppUserId, uv.VisitId }));
+
+            // Define the relationship for UserVisits: user has visits
+            builder.Entity<UserVisit>()
+                .HasOne(u => u.AppUser)
+                .WithMany(v => v.UserVisits)
+                .HasForeignKey(u => u.AppUserId);
+
+            // Define the relationship for UserVisits: visit has users
+            builder.Entity<UserVisit>()
+                .HasOne(v => v.Visit)
+                .WithMany(u => u.UserVisits)
+                .HasForeignKey(v => v.VisitId);
         }
     }
 }
