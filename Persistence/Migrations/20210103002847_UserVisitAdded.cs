c572d2c65079f793d2fdc8db02aee932f5b1bb16
unknown <alia.azarian@gmail.com>
2021-01-03 00:32:17 +0000

    Migration added - new table called UserVisits added to the Pinda database

---
+++ Persistence/Migrations/20210103002847_UserVisitAdded.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class UserVisitAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserVisits",
+                columns: table => new
+                {
+                    AppUserId = table.Column<string>(nullable: false),
+                    VisitId = table.Column<Guid>(nullable: false),
+                    DateJoined = table.Column<DateTime>(nullable: false),
+                    IsHost = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserVisits", x => new { x.AppUserId, x.VisitId });
+                    table.ForeignKey(
+                        name: "FK_UserVisits_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserVisits_Visits_VisitId",
+                        column: x => x.VisitId,
+                        principalTable: "Visits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserVisits_VisitId",
+                table: "UserVisits",
+                column: "VisitId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserVisits");
+        }
+    }
+}
