fe9262824c0b901bdcac816851e1de12d9fe0870
unknown <alia.azarian@gmail.com>
2021-01-04 00:01:37 +0000

    Updated VisitsController by adding Authorize decorator tags to Edit and Delete

--- API/Controllers/VisitsController.cs
+++ API/Controllers/VisitsController.cs
@@ -36,6 +36,7 @@ namespace API.Controllers
 
         //  For users to edit a database item
         [HttpPut("{id}")]
+        [Authorize(Policy = "IsActivityHost")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
             command.Id = id;
@@ -44,6 +45,7 @@ namespace API.Controllers
 
         // For users to delete database items 
         [HttpDelete("{id}")]
+        [Authorize(Policy = "IsActivityHost")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
             return await Mediator.Send(new Delete.Command { Id = id });
