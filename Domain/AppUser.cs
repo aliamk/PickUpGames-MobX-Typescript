335533157f7ee1921d8749fc679cdfe5f370d6e6
unknown <alia.azarian@gmail.com>
2021-01-05 22:35:49 +0000

    Updated AppUser with Photos and added Bio for now because will be building that soon

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -6,6 +6,11 @@ namespace Domain
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
+
+        public string Bio { get; set; }
         public virtual ICollection<UserVisit> UserVisits { get; set; }
+        public virtual ICollection<Photo> Photos { get; set; }
     }
 }
+
+// virtual = lazy loading
