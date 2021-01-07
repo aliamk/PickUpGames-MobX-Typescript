10b5e375d4d17858adb3a6f05b0246de311c5f3f
unknown <alia.azarian@gmail.com>
2021-01-07 23:26:18 +0000

    Updated PhotoUploadWidget with a check to see if a file exists so it can be previewed in section 3 - this is now working

--- client-app/src/App/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/App/common/photoUpload/PhotoUploadWidget.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { Header, Grid, Button } from 'semantic-ui-react';
+import { Header, Grid, Image } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import PhotoWidgetDropzone from './PhotoWidgetDropZone'
 
@@ -24,24 +24,7 @@ const PhotoUploadWidget = () => {
         <Grid.Column width={4}>
         {/* SECTION THREE */}
           <Header sub color='teal' content='Step 3 - Preview & Upload' />
-          {files.length > 0 && (
-            <Fragment>
-              <div
-                className='img-preview'
-                style={{ minHeight: '200px', overflow: 'hidden' }}
-              />
-              <Button.Group widths={2}>
-                <Button
-                  positive
-                  icon='check'
-                />
-                <Button
-                  icon='close'                
-                  onClick={() => setFiles([])}
-                />
-              </Button.Group>
-            </Fragment>
-          )}
+          {files.length > 0 && <Image src={files[0].preview} /> }
         </Grid.Column>
       </Grid>
     </Fragment>
@@ -74,3 +57,22 @@ export default observer(PhotoUploadWidget);
               imagePreview={files[0].preview}
             />
           )} */
+
+        //   {files.length > 0 && (
+        //     <Fragment>
+        //       <div
+        //         className='img-preview'
+        //         style={{ minHeight: '200px', overflow: 'hidden' }}
+        //       />
+        //       <Button.Group widths={2}>
+        //         <Button
+        //           positive
+        //           icon='check'
+        //         />
+        //         <Button
+        //           icon='close'                
+        //           onClick={() => setFiles([])}
+        //         />
+        //       </Button.Group>
+        //     </Fragment>
+        //   )}
