edb4c3b7898817b627aa2770eef5168ea8b3854f
unknown <alia.azarian@gmail.com>
2021-01-07 23:40:05 +0000

    Added two style object for the dropzone in PhotoWidgetDropZone: the default will be a grey dotted border but when user drags an image file over it then the second style should override the grey color with green. Also added an icon and text inside the dropzone.

--- client-app/src/App/common/photoUpload/PhotoWidgetDropZone.tsx
+++ client-app/src/App/common/photoUpload/PhotoWidgetDropZone.tsx
@@ -1,9 +1,24 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { Header, Icon } from 'semantic-ui-react';
 
 interface IProps {
     setFiles: (files: object[]) => void;
-  }
+}
+
+const dropzoneStyles = {
+    border: 'dashed 3px',
+    borderColor: '#eee',
+    borderRadius: '5px',
+    paddingTop: '30px',
+    textAlign: 'center' as 'center', // using 'as' to get around the typescript error
+    height: '200px'
+};
+
+// When dropzone is active, apply the above style but override the b color with green
+const dropzoneActive = {
+    borderColor: 'green'
+};
 
 const PhotoWidgetDropzone: React.FC<IProps> = ({ setFiles }) => {
 
@@ -16,18 +31,23 @@ const PhotoWidgetDropzone: React.FC<IProps> = ({ setFiles }) => {
           })));
     }, [setFiles]);
   
+// The props in userDropzone get references to the drop area
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        {
-        isDragActive ? 
-            <p>Drop the files here</p> : 
-            <p>Drag 'n' drop files here, or click to select files</p>
-        }      
+    <div
+      {...getRootProps()}
+        // Spreading/assigning both style objects so when hovering image over the dropzone
+        // want to apply styles but override the b. color with green
+      style={
+        isDragActive ? { ...dropzoneStyles, ...dropzoneActive } : dropzoneStyles
+      }
+    >
+      <input {...getInputProps()} />
+      <Icon name='upload' size='huge' />
+      <Header content='Drop image here' />
     </div>
-  )
+  );
 }
 
 export default PhotoWidgetDropzone;
