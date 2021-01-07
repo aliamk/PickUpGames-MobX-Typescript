9e0fa5ca505265f09c92001a419301bee4e4a09c
unknown <alia.azarian@gmail.com>
2021-01-07 14:22:58 +0000

    Updated rootStore to connect profileStore

--- client-app/src/App/stores/rootStore.ts
+++ client-app/src/App/stores/rootStore.ts
@@ -4,7 +4,7 @@ import { createContext } from 'react';
 import { configure } from 'mobx';
 import CommonStore from './commonStore';
 import ModalStore from './modalStore';
-// import ProfileStore from './profileStore';
+import ProfileStore from './profileStore';
 
 // Pass the RootStore as a parameter to a constructor
 // Initialise the stores accessible via RootStore
@@ -18,14 +18,14 @@ export class RootStore {
     userStore: UserStore;
     commonStore: CommonStore;
     modalStore: ModalStore;
-    // profileStore: ProfileStore;
+    profileStore: ProfileStore;
 
     constructor() {
         this.visitStore = new VisitStore(this);
         this.userStore = new UserStore(this);
         this.commonStore = new CommonStore(this);
         this.modalStore = new ModalStore(this);
-        // this.profileStore = new ProfileStore(this);
+        this.profileStore = new ProfileStore(this);
     }
 }
 
