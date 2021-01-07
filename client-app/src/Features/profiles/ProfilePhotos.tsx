040948e4fd98c6e6610594c301aba25da0806ec6
unknown <alia.azarian@gmail.com>
2021-01-07 17:39:04 +0000

    Created a ProfilePhotos component in Features > profiles > ProfilePhotos that will display all the photos a user has uploaded on their profile page.

---
+++ client-app/src/Features/profiles/ProfilePhotos.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import {Tab, Grid, Header, Button, Card, Image} from 'semantic-ui-react'
+import { RootStoreContext } from '../../app/stores/rootStore';
+
+
+const ProfilePhotos = () => {
+    // Access the profile by bringing in the RootStoreContext and destructuring
+    // the profileStore from it
+    const rootStore = useContext(RootStoreContext);
+    const { profile } = rootStore.profileStore
+    return (
+        <Tab.Pane>
+        <Grid>
+            <Header floated='left' icon='image' content='Photos' />
+            <Card.Group itemsPerRow={5}>
+            {/* If profile is not null... */}
+                {profile &&
+                  profile.photos.map(photo => (
+                    <Card key={photo.id}>
+                      <Image src={photo.url} />                    
+                    </Card>
+                 ))}
+              </Card.Group>     
+        </Grid>
+      </Tab.Pane>
+    )
+}
+
+export default ProfilePhotos
+
+            {/* {isCurrentUser && (
+              <Button
+                onClick={() => setAddPhotoMode(!addPhotoMode)}
+                floated='right'
+                basic
+                content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+              />
+            )} */}
