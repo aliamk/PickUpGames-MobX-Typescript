8c2f3d27404db9578579fb05700842b95b0c72bb
unknown <alia.azarian@gmail.com>
2021-01-07 14:36:33 +0000

    Update ProfilePage with a RouteParams, Iprops, the match property as prop, a useEffect function for getting the username and pass 'profile' as prop to ProfileHeader

--- client-app/src/Features/profiles/ProfilePage.tsx
+++ client-app/src/Features/profiles/ProfilePage.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import { Grid } from 'semantic-ui-react'
 import ProfileHeader from './ProfileHeader'
 import ProfileContent from './ProfileContent'
+import { RootStoreContext } from '../../App/stores/rootStore'
+import { RouteComponentProps } from 'react-router-dom'
+import LoadingComponent from '../../App/layout/LoadingComponent'
 
-const ProfilePage = () => {
+interface RouteParams {
+    username: string;
+  }
+  
+interface IProps extends RouteComponentProps<RouteParams> {}
+
+const ProfilePage: React.FC<IProps> = ({ match }) => {
+    const rootStore = useContext(RootStoreContext)
+    const {loadingProfile, profile, loadProfile} = rootStore.profileStore;
+
+    useEffect(() => {
+        loadProfile(match.params.username);
+      }, [loadProfile, match]);
+
+    if (loadingProfile) return <LoadingComponent content='Loading profile...' />;
+
     return (
         <div>
             <Grid.Column width={16}>
-                <ProfileHeader />
+                <ProfileHeader profile={profile}/>
                 <ProfileContent />
             </Grid.Column>
         </div>
