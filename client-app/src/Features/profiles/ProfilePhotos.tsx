8fbf1651875b24d4c9eba06428e4d096854a9f76
unknown <alia.azarian@gmail.com>
2021-01-07 19:22:33 +0000

    Updated ProfilePhotos so that photos show by default, the Add Photo button toggles between Add/Cancel, the Add option clears the photos for spave for a photo widget that I'll add next; also added two buttons below each photo: one for setting a photo as the main and one for deleting photos

--- client-app/src/Features/profiles/ProfilePhotos.tsx
+++ client-app/src/Features/profiles/ProfilePhotos.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import {Tab, Grid, Header, Button, Card, Image} from 'semantic-ui-react'
 import { RootStoreContext } from '../../App/stores/rootStore';
+// import PhotoUploadWidget from '../../app/common/photoUpload/PhotoUploadWidget';
 
 const ProfilePhotos = () => {
     // Access the profile by bringing in the RootStoreContext and destructuring
@@ -12,41 +13,59 @@ const ProfilePhotos = () => {
     return (
         <Tab.Pane>
         <Grid>
+
             <Grid.Column width={16} style={{ paddingBottom: 0 }}>
                 <Header floated='left' icon='image' content='Photos' />
                     {/* If isCurrentUser is true, show this button */}
                     {isCurrentUser && (
                         <Button
-                            onClick={() => setAddPhotoMode(!addPhotoMode)}
+                            onClick={() => setAddPhotoMode(!addPhotoMode)} // toggle 'Add' / 'Cancel'
                             floated='right'
                             basic
                             content={addPhotoMode ? 'Cancel' : 'Add Photo'}
                         />
                     )}
             </Grid.Column>
+
             <Grid.Column width={16}>
+                {/* If addPhotoMode is true, show the paragraph, else show photos */}
+                {addPhotoMode ? (
+                    <p>Photo widget will go here</p>
+                ) : (
                 <Card.Group itemsPerRow={5}>
                     {/* If profile is not null, show the photos */}
                     {profile &&
                         profile.photos.map(photo => (
                             <Card key={photo.id}>
-                            <Image src={photo.url} />                    
+                            <Image src={photo.url} />  
+                            {isCurrentUser && (
+                                <Button.Group fluid widths={2}>
+                                    <Button
+                                        onClick={() => setAddPhotoMode(!addPhotoMode)} 
+                                        name={photo.id}
+                                        disabled={photo.isMain}
+                                        basic
+                                        positive
+                                        content='Main'
+                                    />
+                                    <Button
+                                        name={photo.id}
+                                        disabled={photo.isMain}
+                                        basic
+                                        negative
+                                        icon='trash'
+                                    />
+                                </Button.Group>
+                                )}                  
                             </Card>
                         ))}
-                </Card.Group>   
-            </Grid.Column>  
+                </Card.Group> 
+                )}  
+            </Grid.Column>
+
         </Grid>
       </Tab.Pane>
     )
 }
 
 export default ProfilePhotos
-
-            {/* {isCurrentUser && (
-              <Button
-                onClick={() => setAddPhotoMode(!addPhotoMode)}
-                floated='right'
-                basic
-                content={addPhotoMode ? 'Cancel' : 'Add Photo'}
-              />
-            )} */}
