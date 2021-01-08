90543df4e40e42e596e1532a34970edab1b7bd74
unknown <alia.azarian@gmail.com>
2021-01-08 13:04:53 +0000

    Added an Edit file in Application > Profiles so the user can edit their bio

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -6,7 +6,6 @@ namespace Domain
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
-
         public string Bio { get; set; }
         public virtual ICollection<UserVisit> UserVisits { get; set; }
         public virtual ICollection<Photo> Photos { get; set; }
