4d1d51b49a78b59214ae0512de7c35a8e4fb21fc
unknown <alia.azarian@gmail.com>
2021-01-08 00:44:41 +0000

    In PhotoWidgetCropper, added IProps interface, copied starter code from React-Cropper site and pasted it into the return statement, created a useRef hook for getting access to the cropper element in the DOM, created a function for checking the reference exists or not- if it does, then set it but if not, get it and convert it to a blob and then set it.  Added all the necessary Cropper properties to make the resizing element work.

--- client-app/src/App/common/photoUpload/PhotoWidgetCropper.tsx
+++ client-app/src/App/common/photoUpload/PhotoWidgetCropper.tsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 
-const PhotoWidgetCropper = () => {
+interface IProps {
+    setImage: (file: Blob) => void;
+    imagePreview: string;
+  }  
+
+const PhotoWidgetCropper: React.FC<IProps> = ({ setImage, imagePreview }) => {
+
+    // Get reference to the cropper element from the DOM
+    const cropper = useRef(null);
+
+    const cropImage = () => {
+        if (
+            // Does the cropper ref exist? If yes, just return
+          cropper.current && typeof cropper.current.getCroppedCanvas() === 'undefined') {
+          return;
+        }
+        // Else, get the cropper object and convert it to a blob
+        cropper && cropper.current && cropper.current.getCroppedCanvas().toBlob((blob: any) => {
+            // set image state to blob with type of image or jpg
+            setImage(blob); 
+          }, 
+          'image/jpeg');
+      };
+
     return (
         <Cropper
-            src="http://fengyuanchen.github.io/cropper/images/picture.jpg"
+            ref={cropper}
+            src={imagePreview}
             style={{ height: 400, width: '100%' }}
             // Cropper.js options
-            aspectRatio={16 / 9}
+            aspectRatio={1 / 1}
+            preview='.img-preview'
             guides={false}
-            crop={this._crop.bind(this)}
-            onInitialized={this.onCropperInit.bind(this)}
+            viewMode={1}
+            dragMode='move'
+            scalable={true}
+            cropBoxMovable={true}
+            cropBoxResizable={true}
+            crop={cropImage}
         />
     )
 }
