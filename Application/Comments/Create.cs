49b39cf6d5c3415c5bcd0a18a00b4bac59822261
unknown <alia.azarian@gmail.com>
2021-01-08 15:12:05 +0000

    Created Create.cs in Application > Comments for the handler function which will add the comments to the Comments object and the updated CommentDto will be returned

---
+++ Application/Comments/Create.cs
@@ -0,0 +1,85 @@
+using System;                           // Exception
+using System.Threading;                 // CancellationToken
+using System.Threading.Tasks;           // Task
+using Application.Interfaces;
+using Domain;
+using FluentValidation;
+using MediatR;                          // IRequest
+using Microsoft.EntityFrameworkCore;
+using Persistence;                      // DataContext
+
+namespace Application.Visits
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            // public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                // RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var visit = new Visit
+                {
+                    Id = request.Id,
+                    Title = request.Title,
+                    Description = request.Description,
+                    // Category = request.Category,
+                    Date = request.Date,
+                    City = request.City,
+                    Venue = request.Venue
+                };
+
+                _context.Visits.Add(visit);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.GetCurrentUsername());
+
+                var attendee = new UserVisit
+                {
+                    AppUser = user,
+                    Visit = visit,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserVisits.Add(attendee);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
