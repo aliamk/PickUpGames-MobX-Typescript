64fe347f0d7804088cb56dbb5bba87166aa6e95b
unknown <alia.azarian@gmail.com>
2021-01-08 00:09:07 +0000

    Created a component for the PhotoWidgetCropper (got code from it's website), updated PhotoUploadWidget with a useState that sets/updates the image and passed the PhotoWidgetCropper component as prop to the header of section 2

--- client-app/src/App/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/App/common/photoUpload/PhotoUploadWidget.tsx
@@ -18,13 +18,13 @@ const PhotoUploadWidget = () => {
   return (
     <Fragment>
       <Grid>
-        {/* SECTION ONE */}
+        {/* SECTION ONE - Drop Selected Photo */}
         <Grid.Column width={4}>
           <Header color='teal' sub content='Step 1 - Add Photo' />  
         <PhotoWidgetDropzone setFiles={setFiles} />        
         </Grid.Column>
 
-        {/* SECTION TWO */}
+        {/* SECTION TWO - Resize Photo*/}
         <Grid.Column width={1} />
         <Grid.Column width={4}>
           <Header sub color='teal' content='Step 2 - Resize image' />
@@ -38,7 +38,7 @@ const PhotoUploadWidget = () => {
         <Grid.Column width={1} />
         <Grid.Column width={4}>
 
-        {/* SECTION THREE */}
+        {/* SECTION THREE - Preview Image */}
           <Header sub color='teal' content='Step 3 - Preview & Upload' />
           {files.length > 0 && <Image src={files[0].preview} /> }
         </Grid.Column>
--- client-app/src/App/common/photoUpload/PhotoWidgetCropper.tsx
+++ client-app/src/App/common/photoUpload/PhotoWidgetCropper.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
 
 const PhotoWidgetCropper = () => {
     return (
-        <div>
-            
-        </div>
+        <Cropper
+            src="http://fengyuanchen.github.io/cropper/images/picture.jpg"
+            style={{ height: 400, width: '100%' }}
+            // Cropper.js options
+            aspectRatio={16 / 9}
+            guides={false}
+            crop={this._crop.bind(this)}
+            onInitialized={this.onCropperInit.bind(this)}
+        />
     )
 }
 
