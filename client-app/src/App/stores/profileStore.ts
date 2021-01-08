e03ff08800c73cdafe98d4e4f9e57d4ac3c4784e
unknown <alia.azarian@gmail.com>
2021-01-08 02:34:59 +0000

    Updated profileStore with a new action called uploadPhoto that allows async photo uploads

--- client-app/src/App/stores/profileStore.ts
+++ client-app/src/App/stores/profileStore.ts
@@ -2,6 +2,7 @@ import { RootStore } from './rootStore';
 import { observable, action, runInAction, computed } from 'mobx';
 import { IProfile } from '../models/profile';
 import agent from '../api/agent';
+import { toast } from 'react-toastify';
 
 
 export default class ProfileStore {
@@ -12,6 +13,8 @@ export default class ProfileStore {
 
   @observable profile: IProfile | null = null;
   @observable loadingProfile = true;
+  @observable uploadingPhoto = false;
+
 
   @computed get isCurrentUser() {
     if (this.rootStore.userStore.user && this.profile) {
@@ -36,5 +39,32 @@ export default class ProfileStore {
       });
       console.log(error);
     }
+  }
+
+  // Method for uploading an image
+  @action uploadPhoto = async (file: Blob) => {
+    this.uploadingPhoto = true;
+    try {
+      const photo = await agent.Profiles.uploadPhoto(file);
+      runInAction(() => {
+        if (this.profile) {                                         // if profile exists push it to photos
+          this.profile.photos.push(photo);
+          if (photo.isMain && this.rootStore.userStore.user) {      // if photo isMain and user exists in the userStore
+            this.rootStore.userStore.user.image = photo.url;
+            this.profile.image = photo.url;
+          }
+        }
+        this.uploadingPhoto = false;
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error('Problem uploading photo');
+      runInAction(() => {
+        this.uploadingPhoto = false;
+      });
+    }
   };
-}
+
+
+} // End ProfileStore
+
