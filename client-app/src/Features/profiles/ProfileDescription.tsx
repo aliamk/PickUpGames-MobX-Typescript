794a08b11391bae9565ff268f332ab869b9d4034
unknown <alia.azarian@gmail.com>
2021-01-08 14:01:35 +0000

    Wanted the display to switch from edit mode to display once user has clicked Update profile so added a handleEditForm handler method in ProfileDescription and passed it to ProfileEditForm as a prop

--- client-app/src/Features/profiles/ProfileDescription.tsx
+++ client-app/src/Features/profiles/ProfileDescription.tsx
@@ -3,11 +3,19 @@ import { Tab, Grid, Header, Button } from 'semantic-ui-react';
 import { RootStoreContext } from '../../App/stores/rootStore';
 import ProfileEditForm from './ProfileEditForm';
 import { observer } from 'mobx-react-lite';
+import { IProfile } from '../../App/models/profile';
 
 const ProfileDescription = () => {
   const rootStore = useContext(RootStoreContext);
   const { updateProfile, profile, isCurrentUser } = rootStore.profileStore;
   const [editMode, setEditMode] = useState(false);
+
+// Once UpdateProfile button is pressed, setState to false so the form goes away
+const handleEditForm = (profile: Partial<IProfile>) => {
+    updateProfile(profile).then(() => setEditMode(false));
+    };
+    
+
   return (    
     <Tab.Pane>                      {/* Update the 'panes' array in ProfileContent */}
       <Grid>
@@ -30,7 +38,7 @@ const ProfileDescription = () => {
 
         <Grid.Column width={16}>
           {editMode ? (
-            <ProfileEditForm updateProfile={updateProfile} profile={profile!} />
+            <ProfileEditForm updateProfile={handleEditForm} profile={profile!} />
           ) : (
             <span>{profile!.bio}</span>
           )}
