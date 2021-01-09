79a4c357f94aa6d79845a59daab4fc74a1a97827
unknown <alia.azarian@gmail.com>
2021-01-09 23:26:37 +0000

    Created the ProfileVisits component in Features > profiles

---
+++ client-app/src/Features/profiles/ProfileVisits.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useContext } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Tab, Grid, Header, Card, Image, TabProps } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { IUserVisit } from '../../app/models/profile';
+import { format } from 'date-fns';
+import { RootStoreContext } from '../../app/stores/rootStore';
+
+const panes = [
+  { menuItem: 'Future Events', pane: { key: 'futureEvents' } },
+  { menuItem: 'Past Events', pane: { key: 'pastEvents' } },
+  { menuItem: 'Hosting', pane: { key: 'hosted' } }
+];
+
+const ProfileEvents = () => {
+  const rootStore = useContext(RootStoreContext);
+  const {
+    loadUserVisits,
+    profile,
+    loadingVisits,
+    userVisits
+  } = rootStore.profileStore!;
+
+  useEffect(() => {
+    loadUserVisits(profile!.username);
+  }, [loadUserVisits, profile]);
+
+  const handleTabChange = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    data: TabProps
+  ) => {
+    let predicate;
+    switch (data.activeIndex) {
+      case 1:
+        predicate = 'past';
+        break;
+      case 2:
+        predicate = 'hosting';
+        break;
+      default:
+        predicate = 'future';
+        break;
+    }
+    loadUserVisits(profile!.username, predicate);
+  };
+
+  return (
+    <Tab.Pane loading={loadingVisits}>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated='left' icon='calendar' content={'Visits'} />
+        </Grid.Column>
+        <Grid.Column width={16}>
+          <Tab
+            panes={panes}
+            menu={{ secondary: true, pointing: true }}
+            onTabChange={(e, data) => handleTabChange(e, data)}
+          />
+          <br />
+          <Card.Group itemsPerRow={4}>
+            {userVisits.map((visit: IUserVisit) => (
+              <Card
+                as={Link}
+                to={`/visits/${visit.id}`}
+                key={visit.id}
+              >
+                <Image
+                  src={`/assets/categoryImages/${visit.category}.jpg`}
+                  style={{ minHeight: 100, objectFit: 'cover' }}
+                />
+                <Card.Content>
+                  <Card.Header textAlign='center'>{visit.title}</Card.Header>
+                  <Card.Meta textAlign='center'>
+                    <div>{format(new Date(visit.date), 'do LLL')}</div>
+                    <div>{format(new Date(visit.date), 'h:mm a')}</div>
+                  </Card.Meta>
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
+
+export default observer(ProfileEvents);
