aa1c84e30e73d141b65b2b0fa38844dadd92d682
unknown <alia.azarian@gmail.com>
2021-01-11 22:59:36 +0000

    Updated Register.cs by replacing the IJwtGenerator with userManager's GenerateEmailConfirmationTokenAsync because don't want to generate tokens for users to log-in straight after registering, only after clicking the confirmation link sent via email, and removing instances of 'User' because Register.cs doesn't need to return a user object, only needs to send an email

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -1,28 +1,33 @@
-using System;                           // Exception
-using System.Linq;                      // FirstOrDefault
-using System.Net;                       // HttpStatusCode
-using System.Threading;                 // CancellationToken
-using System.Threading.Tasks;           // Task
-using Application.Errors;               // RestException
-using Application.Interfaces;           // IJwtGenerator
-using Application.Validators;           // Password
-using Domain;                           // AppUser
-using FluentValidation;                 // AbstractValidator
-using MediatR;                          // IRequest
-using Microsoft.AspNetCore.Identity;    // UserManager
-using Microsoft.EntityFrameworkCore;    // AnyAsync
-using Persistence;                      // DataContext
+using System;                               // Exception
+using System.Linq;                          // FirstOrDefault
+using System.Net;                           // HttpStatusCode
+using System.Text;                          // Encoding
+using System.Threading;                     // CancellationToken
+using System.Threading.Tasks;               // Task
+using Application.Errors;                   // RestException
+using Application.Interfaces;               // IJwtGenerator
+using Application.Validators;               // Password
+using Domain;                               // AppUser
+using FluentValidation;                     // AbstractValidator
+using MediatR;                              // IRequest
+using Microsoft.AspNetCore.Identity;        // UserManager
+using Microsoft.AspNetCore.WebUtilities;    // WebEncoders
+using Microsoft.EntityFrameworkCore;        // AnyAsync
+using Persistence;                          // DataContext
 
 namespace Application.User
 {
     public class Register
     {
-        public class Command : IRequest<User>
+        public class Command : IRequest
         {
             public string DisplayName { get; set; }
             public string Username { get; set; }
             public string Email { get; set; }
             public string Password { get; set; }
+
+            // Get origin of email from the API endpoints header (to use with registration email token)
+            public string Origin { get; set; }
         }
 
         public class CommandValidator : AbstractValidator<Command>
@@ -36,19 +41,19 @@ namespace Application.User
             }
         }
 
-        public class Handler : IRequestHandler<Command, User>
+        public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
             private readonly UserManager<AppUser> _userManager;
-            private readonly IJwtGenerator _jwtGenerator;
-            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
+            private readonly IEmailSender _emailSender;
+            public Handler(DataContext context, UserManager<AppUser> userManager, IEmailSender emailSender)
             {
-                _jwtGenerator = jwtGenerator;
+                _emailSender = emailSender;
                 _userManager = userManager;
                 _context = context;
             }
 
-            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 // handler logic
                 if (await _context.Users.AnyAsync(x => x.Email == request.Email))
@@ -64,19 +69,23 @@ namespace Application.User
                     UserName = request.Username
                 };
 
+                // Create new user if request is successful
                 var result = await _userManager.CreateAsync(user, request.Password);
-                if (result.Succeeded)
-                {
-                    return new User
-                    {
-                        DisplayName = user.DisplayName,
-                        Token = _jwtGenerator.CreateToken(user),
-                        Username = user.UserName,
-                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
-                    };
-                }
+                // Don't create new user if request is NOT successful
+                if (!result.Succeeded) throw new Exception("Problem creating user");
 
-                throw new Exception("Problem creating user");
+                // Generate a token to send via email and pass in the user
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                // Generates a token in pure string format, not query string
+                token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+                // What the confirm registration url should look like in the email
+                var verifyUrl = $"{request.Origin}/user/verifyEmail?token={token}&email={request.Email}";
+                // What the email should look like
+                var message = $"<p>Please click the below link to verify your email address:</p><p><a href='{verifyUrl}'>{verifyUrl}></a></p>";
+                // Replace the JwtGenerator with this method
+                await _emailSender.SendEmailAsync(request.Email, "Please verify email address", message);
+
+                return Unit.Value;
             }
         }
     }
