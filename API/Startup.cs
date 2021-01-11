6bde741fb1a180a89af913d859acf694c4ab40b9
unknown <alia.azarian@gmail.com>
2021-01-11 18:12:25 +0000

    Updated Startup again with Addscroped services

--- API/Startup.cs
+++ API/Startup.cs
@@ -126,6 +126,7 @@ namespace API
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.AddScoped<IEmailSender, EmailSender>();
             services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
             services.Configure<SendGridSettings>(Configuration.GetSection("SendGrid"));
         }
