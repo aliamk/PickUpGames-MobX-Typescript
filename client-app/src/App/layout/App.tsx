f84896482078bbbad3fd62910765f1cf30e38025
unknown <alia.azarian@gmail.com>
2021-01-12 19:54:30 +0000

    Create a verifyEmail component in Features > User.  It handles situation when user's verification is ongoing, failed or succeeded

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -14,7 +14,8 @@ import LoadingComponent from './LoadingComponent'
 import ModalContainer from '../common/modals/ModalContainer'
 import ProfilePage from '../../Features/profiles/ProfilePage'
 import PrivateRoute from './PrivateRoute'
-
+import RegisterSuccess from '../../../src/Features/user/RegisterSuccess'
+import VerifyEmail from '../../../src/Features/user/VerifyEmail'
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {  
 
@@ -48,6 +49,8 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                 {/* Using location:key to create a new instance of the loaded component when a prop changes  */}
                 <PrivateRoute key={location.key} path={['/createVisit', '/manage/:id']} component={VisitForm} />  
                 <PrivateRoute path='/profile/:username' component={ProfilePage} />
+                <PrivateRoute path='/user/registerSuccess' component={RegisterSuccess} />
+                <PrivateRoute path='/user/verifyEmail' component={VerifyEmail} />
                 {/* <Route path='/login' component={LoginForm} /> */}
                 <Route component={NotFound} />    
               </Switch>
